0c9a1a531332c94db958bc5bcd0e1dbe55ae83d5
JuanfeRomero <juanferomerocode@gmail.com>
2021-10-25 09:37:49 -0300

    Agregar archivos de proyecto.

---
+++ Controllers/ProvinciaController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Web;
+using System.Web.Mvc;
+using MVCApplication.Models;
+
+namespace MVCApplication.Controllers
+{
+    public class ProvinciaController : Controller
+    {
+        IList<ProvinciaModel> provincias = new List<ProvinciaModel>()
+        {
+            new ProvinciaModel() { Id = 1, Descripcion = "Buenos Aires" },
+            new ProvinciaModel() { Id = 2, Descripcion = "Córdoba" },
+            new ProvinciaModel() { Id = 3, Descripcion = "Entre Ríos"}
+        };
+
+        public string CargarPopup()
+        {
+            DatosPopup datos = new DatosPopup();
+            datos.InSitu = true;
+            datos.Tercerizado = false;
+            datos.TiposTrabajo = new List<TipoTrabajo>();
+            datos.TiposTrabajo.Add(new TipoTrabajo(){Id=1, Descripcion = "Tipo1"});
+            datos.TiposTrabajo.Add(new TipoTrabajo(){Id=2, Descripcion = "Tipo2"});
+            datos.TiposTrabajo.Add(new TipoTrabajo(){Id=3, Descripcion = "Tipo3"});
+
+            System.IO.StringWriter sw = new System.IO.StringWriter();
+            ViewEngineResult ver = ViewEngines.Engines.FindPartialView(this.ControllerContext, "DatosPopup");
+            this.ViewData.Model = datos;
+            ViewContext vc = new ViewContext(this.ControllerContext, ver.View, this.ViewData, this.TempData, sw);
+
+            ver.View.Render(vc, sw);
+            return sw.GetStringBuilder().ToString();
+        }
+
+        public string GuardarDatosPopup(DatosPopup datosPopup)
+        {
+            string respuesta = "";
+
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    respuesta = "TODO OK!"; // TODO: realizar las operaciones necesarias de la ViewModel
+                }
+            }
+            catch (Exception ex)
+            {
+                respuesta = ex.Message;
+            }
+
+            return respuesta;
+        }
+        
+        // GET: Provincia
+        public ActionResult Index()
+        {
+            IList<ProvinciaModel> provincias = this.provincias;
+            
+            return View(provincias);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int idProvincia)
+        {
+            IList<ProvinciaModel> provincias = this.provincias;
+
+            //!!!! Esto deberia resolverse con la consulta al negocio, es solo de ejemplo
+            ProvinciaModel provincia = (
+                    from prov in provincias
+                    where prov.Id == idProvincia
+                    select prov).First();
+
+            return View("Edit", provincia);
+        }
+        [HttpPost]
+        public ActionResult Edit(ProvinciaModel provincia)
+        {
+            ProvinciaModel aReemplazar = this.provincias.FirstOrDefault(e => e.Id == provincia.Id);
+            if (ModelState.IsValid || aReemplazar == null)
+            {
+                // Aqui iria el codigo para almacenar los cambios
+                // simplemente voy a modificar la lista del metodo
+                int index = this.provincias.IndexOf(aReemplazar);
+                this.provincias[index] = provincia;
+                return RedirectToAction("Index");
+            }
+
+            return View("Edit", provincia);
+        }
+        [HttpGet]
+        public ActionResult Delete(int idProvincia)
+        {
+            IList<ProvinciaModel> provincias = this.provincias;
+
+            //!!!! Esto deberia resolverse con la consulta al negocio, es solo de ejemplo
+            ProvinciaModel provincia = (
+                from prov in provincias
+                where prov.Id == idProvincia
+                select prov).First();
+            
+            return View("Delete", provincia);
+
+        }
+        [HttpPost]
+        public ActionResult Delete(ProvinciaModel provincia)
+        {
+            IList<ProvinciaModel> provincias = this.provincias;
+            ProvinciaModel aBorrar = this.provincias.FirstOrDefault(e => e.Id == provincia.Id);
+
+            this.provincias.RemoveAt(this.provincias.IndexOf(aBorrar));
+
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ Models/ProvinciaModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace MVCApplication.Models
+{
+    public class ProvinciaModel
+    {
+        [Required]
+        [CustomValidation(typeof(ProvinciaValidations), "IsValidIdProvincia")]
+        [IsValidIdProvinciaAttributte(ErrorMessage="El id de la provincia es invalido")]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name="NombreProvincia", ResourceType = typeof(Resources.Global.Global))]
+        [StringLength(20)]
+        public string Descripcion { get; set; }
+    }
+
+    internal class IsValidIdProvinciaAttributte : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            int id = Convert.ToInt32(value);
+            return id > 0;
+        }
+    }
+
+    internal static class ProvinciaValidations
+    {
+        public static ValidationResult IsValidIdProvincia(int value)
+        {
+            if (value > 0)
+            {
+                return ValidationResult.Success;
+            }
+            else
+            {
+                return new ValidationResult("El id de la provincia debe ser mayor a 0");
+            }
+        }
+    }
+}
